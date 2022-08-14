ca2cf4fc2071e7ff5427ba0970cf3d0a36ad6abd
unknown <mehedi15-2995@diu.edu.bd>
2022-08-14 18:50:42 +0600

    added something

--- js/event.js
+++ js/event.js
@@ -5,7 +5,7 @@ function makeRed (){
     document.body.style.backgroundColor = 'red';
 }
 
-//option------2
+//option------2----this will be needed most
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick =makeBlue;
 
@@ -14,7 +14,30 @@ function makeBlue (){
 } 
 
 
-//Option----3
+//Option----3-----we will use it sometimes
 const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';}
+
+
+
+    //option----->4
+    const pinkButton = document.getElementById('make-pink');
+       pinkButton.addEventListener('click', makePink);
+
+       function makePink (){
+        document.body.style.backgroundColor = 'pink';
+       }
+
+       //option---->4(another).
+       const makeGreenButton = document.getElementById('make-green');
+       makeGreenButton.addEventListener('click',function makeGreen(){
+        document.body.style.backgroundColor = 'green';
+       })
+        
+
+       //option---->4(Final)-----most important!!!!!
+       //    document.getElementById('make-goldenRoad').addEventListener('click',function(){})
+    document.getElementById('make-goldenRoad').addEventListener('click',function(){
+        document.body.style.backgroundColor = 'goldenrod';
+    })
